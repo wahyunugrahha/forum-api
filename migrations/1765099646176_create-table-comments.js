9987d89c58b0d8a906466729e9c63dad4c8121b2
wahyunugrahha <wahyunih297@gmail.com>
2025-12-07 16:36:24 +0700

    first commit

---
+++ migrations/1765099646176_create-table-comments.js
@@ -0,0 +1,48 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("comments", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    owner: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    thread_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    content: {
+      type: "TEXT",
+      notNull: true,
+    },
+    is_delete: {
+      type: "BOOLEAN",
+      notNull: true,
+      default: pgm.func("FALSE"),
+    },
+    date: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+
+  pgm.addConstraint(
+    "comments",
+    "fk_comments.owner_users.id",
+    "FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE"
+  );
+
+  pgm.addConstraint(
+    "comments",
+    "fk_comments.thread_id_threads.id",
+    "FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE"
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("comments");
+};
---
+++ migrations/1765099658735_create-table-replies.js
@@ -0,0 +1,58 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("replies", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    owner: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    comment_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    thread_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    content: {
+      type: "TEXT",
+      notNull: true,
+    },
+    is_delete: {
+      type: "BOOLEAN",
+      notNull: true,
+      default: pgm.func("FALSE"),
+    },
+    date: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+
+  pgm.addConstraint(
+    "replies",
+    "fk_replies.owner_users.id",
+    "FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE"
+  );
+
+  pgm.addConstraint(
+    "replies",
+    "fk_replies.comment_id_comments.id",
+    "FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE"
+  );
+
+  pgm.addConstraint(
+    "replies",
+    "fk_replies.thread_id_threads.id",
+    "FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE"
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("replies");
+};
